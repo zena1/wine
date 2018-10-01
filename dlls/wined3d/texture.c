0ba2d4b09099275d14e49cb4a3df25611e4630f8
Tk-Glitch <ti3nou@gmail.com>
2018-10-01 19:39:32 +0200

    PBA fixup for 3.17

--- dlls/wined3d/texture.c
+++ dlls/wined3d/texture.c
@@ -2258,6 +2258,12 @@ static void wined3d_texture_unload(struct wined3d_resource *resource)
 static HRESULT texture_resource_sub_resource_map(struct wined3d_resource *resource, unsigned int sub_resource_idx,
         struct wined3d_map_desc *map_desc, const struct wined3d_box *box, DWORD flags)
 {
+    return E_NOTIMPL;
+}
+
+static HRESULT texture_resource_sub_resource_map_cs(struct wined3d_resource *resource, unsigned int sub_resource_idx,
+        struct wined3d_map_desc *map_desc, const struct wined3d_box *box, DWORD flags)
+{
     const struct wined3d_format *format = resource->format;
     struct wined3d_texture_sub_resource *sub_resource;
     struct wined3d_device *device = resource->device;
@@ -2418,6 +2424,11 @@ static HRESULT texture_resource_sub_resource_map_info(struct wined3d_resource *r
 
 static HRESULT texture_resource_sub_resource_unmap(struct wined3d_resource *resource, unsigned int sub_resource_idx)
 {
+    return E_NOTIMPL;
+}
+
+static HRESULT texture_resource_sub_resource_unmap_cs(struct wined3d_resource *resource, unsigned int sub_resource_idx)
+{
     struct wined3d_texture_sub_resource *sub_resource;
     struct wined3d_device *device = resource->device;
     struct wined3d_context *context = NULL;
@@ -2469,6 +2480,8 @@ static const struct wined3d_resource_ops texture_resource_ops =
     texture_resource_sub_resource_map,
     texture_resource_sub_resource_map_info,
     texture_resource_sub_resource_unmap,
+    texture_resource_sub_resource_map_cs,
+    texture_resource_sub_resource_unmap_cs,
 };
 
 /* Context activation is done by the caller. */
