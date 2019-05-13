zena1/wine

commit 5ff326f80147f4608fd64ad37510d0cff330676e

winebus.sys: Pack hatswitch values into button bytes.
Signed-off-by: Andrew Eikum <aeikum@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/winebus.sys/bus_sdl.c
@@ -123,7 +123,7 @@ struct platform_private
     int button_start;
     int axis_start;
     int ball_start;
-    int hat_start;
+    int hat_bit_offs; /* hatswitches are reported in the same bytes as buttons */
 
     int report_descriptor_size;
     BYTE *report_descriptor;
@@ -164,10 +164,6 @@ static const BYTE CONTROLLER_BUTTONS[] = {
     0x95, CONTROLLER_NUM_BUTTONS, /* REPORT_COUNT (11) */
     0x75, 0x01, /* REPORT_SIZE (1) */
     0x81, 0x02, /* INPUT (Data,Var,Abs) */
-    /* padding */
-    0x95, 0x05, /* REPORT_COUNT (5) */
-    0x75, 0x01, /* REPORT_SIZE (1) */
-    0x81, 0x03, /* INPUT (Cnst,Var,Abs) */
 };
 
 static const BYTE CONTROLLER_AXIS [] = {
@@ -302,22 +298,50 @@ static void set_ball_value(struct platform_private *ext, int index, int value1, 
 
 static void set_hat_value(struct platform_private *ext, int index, int value)
 {
-    int offset;
-    offset = ext->hat_start + index;
+    int byte = ext->button_start + (ext->hat_bit_offs + 4 * index) / 8;
+    int bit_offs = (ext->hat_bit_offs + 4 * index) % 8;
+    int num_low_bits, num_high_bits;
+    unsigned char val, low_mask, high_mask;
+
+    /* 4-bit hatswitch value is packed into button bytes */
+    if (bit_offs <= 4)
+    {
+        num_low_bits = 4;
+        num_high_bits = 0;
+        low_mask = 0xf;
+        high_mask = 0;
+    }
+    else
+    {
+        num_low_bits = 8 - bit_offs;
+        num_high_bits = 4 - num_low_bits;
+        low_mask = (1 << num_low_bits) - 1;
+        high_mask = (1 << num_high_bits) - 1;
+    }
+
     switch (value)
     {
         /* 8 1 2
          * 7 0 3
          * 6 5 4 */
-        case SDL_HAT_CENTERED: ext->report_buffer[offset] = 0; break;
-        case SDL_HAT_UP: ext->report_buffer[offset] = 1; break;
-        case SDL_HAT_RIGHTUP: ext->report_buffer[offset] = 2; break;
-        case SDL_HAT_RIGHT: ext->report_buffer[offset] = 3; break;
-        case SDL_HAT_RIGHTDOWN: ext->report_buffer[offset] = 4; break;
-        case SDL_HAT_DOWN: ext->report_buffer[offset] = 5; break;
-        case SDL_HAT_LEFTDOWN: ext->report_buffer[offset] = 6; break;
-        case SDL_HAT_LEFT: ext->report_buffer[offset] = 7; break;
-        case SDL_HAT_LEFTUP: ext->report_buffer[offset] = 8; break;
+        case SDL_HAT_CENTERED: val = 0; break;
+        case SDL_HAT_UP: val = 1; break;
+        case SDL_HAT_RIGHTUP: val = 2; break;
+        case SDL_HAT_RIGHT: val = 3; break;
+        case SDL_HAT_RIGHTDOWN: val = 4; break;
+        case SDL_HAT_DOWN: val = 5; break;
+        case SDL_HAT_LEFTDOWN: val = 6; break;
+        case SDL_HAT_LEFT: val = 7; break;
+        case SDL_HAT_LEFTUP: val = 8; break;
+        default: return;
+    }
+
+    ext->report_buffer[byte] &= ~(low_mask << bit_offs);
+    ext->report_buffer[byte] |= (val & low_mask) << bit_offs;
+    if (high_mask)
+    {
+        ext->report_buffer[byte + 1] &= ~high_mask;
+        ext->report_buffer[byte + 1] |= val & high_mask;
     }
 }
 
@@ -423,18 +447,16 @@ static BOOL build_report_descriptor(struct platform_private *ext)
     if (button_count)
     {
         descript_size += sizeof(REPORT_BUTTONS);
-        if (button_count % 8)
-            descript_size += sizeof(REPORT_PADDING);
-        report_size += (button_count + 7) / 8;
     }
 
     hat_count = pSDL_JoystickNumHats(ext->sdl_joystick);
-    ext->hat_start = report_size;
+    ext->hat_bit_offs = button_count;
     if (hat_count)
     {
         descript_size += sizeof(REPORT_HATSWITCH);
-        report_size += hat_count;
     }
+
+    report_size += (button_count + hat_count * 4 + 7) / 8;
 
     descript_size += test_haptic(ext);
 
@@ -468,11 +490,6 @@ static BOOL build_report_descriptor(struct platform_private *ext)
     if (button_count)
     {
         report_ptr = add_button_block(report_ptr, 1, button_count);
-        if (button_count % 8)
-        {
-            BYTE padding = 8 - (button_count % 8);
-            report_ptr = add_padding_block(report_ptr, padding);
-        }
     }
     if (hat_count)
         report_ptr = add_hatswitch(report_ptr, hat_count);
@@ -540,9 +557,11 @@ static BOOL build_mapped_report_descriptor(struct platform_private *ext)
 
     ext->axis_start = 0;
     ext->button_start = CONTROLLER_NUM_AXES * sizeof(WORD);
-    ext->hat_start = ext->button_start + (CONTROLLER_NUM_BUTTONS + 7) / 8;
+    ext->hat_bit_offs = CONTROLLER_NUM_BUTTONS;
 
-    ext->buffer_length = (CONTROLLER_NUM_BUTTONS + 7) / 8 + CONTROLLER_NUM_AXES * sizeof(WORD) + CONTROLLER_NUM_HATSWITCHES;
+    ext->buffer_length = (CONTROLLER_NUM_BUTTONS + CONTROLLER_NUM_HATSWITCHES * 4 + 7) / 8
+        + CONTROLLER_NUM_AXES * sizeof(WORD)
+        + 2/* unknown constant*/;
 
     TRACE("Report Descriptor will be %i bytes\n", descript_size);
     TRACE("Report will be %i bytes\n", ext->buffer_length);
@@ -566,6 +585,7 @@ static BOOL build_mapped_report_descriptor(struct platform_private *ext)
     memcpy(report_ptr, CONTROLLER_BUTTONS, sizeof(CONTROLLER_BUTTONS));
     report_ptr += sizeof(CONTROLLER_BUTTONS);
     report_ptr = add_hatswitch(report_ptr, 1);
+    report_ptr = add_padding_block(report_ptr, 16);/* unknown constant */
     report_ptr += build_haptic(ext, report_ptr);
     memcpy(report_ptr, REPORT_TAIL, sizeof(REPORT_TAIL));
 
@@ -583,6 +603,10 @@ static BOOL build_mapped_report_descriptor(struct platform_private *ext)
         set_axis_value(ext, i, pSDL_GameControllerGetAxis(ext->sdl_controller, i));
 
     set_hat_value(ext, 0, compose_dpad_value(ext->sdl_controller));
+
+    /* unknown constant */
+    ext->report_buffer[12] = 0x89;
+    ext->report_buffer[13] = 0xc5;
 
     return TRUE;
 }

dlls/winebus.sys/controller.h
@@ -79,7 +79,7 @@ static const BYTE REPORT_HATSWITCH[] = {
     0x25, 0x08,  /* LOGICAL_MAXIMUM (0x08) */
     0x35, 0x00,  /* PHYSICAL_MINIMUM (0) */
     0x45, 0x08,  /* PHYSICAL_MAXIMUM (8) */
-    0x75, 0x08,  /* REPORT_SIZE (8) */
+    0x75, 0x04,  /* REPORT_SIZE (4) */
     0x95, 0x01,  /* REPORT_COUNT (1) */
     0x81, 0x02,  /* INPUT (Data,Var,Abs) */
 };

commit fe7d66f39322009d53cf69b47eb3744aacd1abe9

Sync with staging r2 for soft link fix, and hotfix gamepad regression introduced by https://github.com/wine-mirror/wine/commit/5ff326f80147f4608fd64ad37510d0cff330676e

dlls/comctl32/static.c
@@ -36,6 +36,7 @@
 #include "winuser.h"
 #include "commctrl.h"
 
+#include "wine/heap.h"
 #include "wine/debug.h"
 
 #include "comctl32.h"
@@ -50,10 +51,17 @@ static void STATIC_PaintBitmapfn( HWND hwnd, HDC hdc, DWORD style );
 static void STATIC_PaintEnhMetafn( HWND hwnd, HDC hdc, DWORD style );
 static void STATIC_PaintEtchedfn( HWND hwnd, HDC hdc, DWORD style );
 
-/* offsets for GetWindowLong for static private information */
-#define HFONT_GWL_OFFSET    0
-#define HICON_GWL_OFFSET    (sizeof(HFONT))
-#define STATIC_EXTRA_BYTES  (HICON_GWL_OFFSET + sizeof(HICON))
+struct static_extra_info
+{
+    HFONT hfont;
+    union
+    {
+        HICON hicon;
+        HBITMAP hbitmap;
+        HENHMETAFILE hemf;
+    } image;
+    BOOL image_has_alpha;
+};
 
 typedef void (*pfPaint)( HWND hwnd, HDC hdc, DWORD style );
 
@@ -79,6 +87,18 @@ static const pfPaint staticPaintFunc[SS_TYPEMASK+1] =
     STATIC_PaintEtchedfn,    /* SS_ETCHEDVERT */
     STATIC_PaintEtchedfn,    /* SS_ETCHEDFRAME */
 };
+
+static struct static_extra_info *get_extra_ptr( HWND hwnd, BOOL force )
+{
+    struct static_extra_info *extra = (struct static_extra_info *)GetWindowLongPtrW( hwnd, 0 );
+    if (!extra && force)
+    {
+        extra = heap_alloc_zero( sizeof(*extra) );
+        if (extra)
+            SetWindowLongPtrW( hwnd, 0, (ULONG_PTR)extra );
+    }
+    return extra;
+}
 
 static BOOL get_icon_size( HICON handle, SIZE *size )
 {
@@ -111,6 +131,7 @@ static HICON STATIC_SetIcon( HWND hwnd, HICON hicon, DWORD style )
 {
     HICON prevIcon;
     SIZE size;
+    struct static_extra_info *extra;
 
     if ((style & SS_TYPEMASK) != SS_ICON) return 0;
     if (hicon && !get_icon_size( hicon, &size ))
@@ -118,7 +139,12 @@ static HICON STATIC_SetIcon( HWND hwnd, HICON hicon, DWORD style )
         WARN("hicon != 0, but invalid\n");
         return 0;
     }
-    prevIcon = (HICON)SetWindowLongPtrW( hwnd, HICON_GWL_OFFSET, (LONG_PTR)hicon );
+
+    extra = get_extra_ptr( hwnd, TRUE );
+    if (!extra) return 0;
+
+    prevIcon = extra->image.hicon;
+    extra->image.hicon = hicon;
     if (hicon && !(style & SS_CENTERIMAGE) && !(style & SS_REALSIZECONTROL))
     {
         /* Windows currently doesn't implement SS_RIGHTJUST */
@@ -138,6 +164,56 @@ static HICON STATIC_SetIcon( HWND hwnd, HICON hicon, DWORD style )
     return prevIcon;
 }
 
+static HBITMAP create_alpha_bitmap( HBITMAP hbitmap )
+{
+    HBITMAP alpha = 0;
+    BITMAPINFO *info = NULL;
+    BITMAP bm;
+    HDC src, dst;
+    void *bits;
+    DWORD i;
+    const unsigned char *ptr;
+    BOOL has_alpha = FALSE;
+
+    if (!GetObjectW( hbitmap, sizeof(bm), &bm )) return 0;
+    if (bm.bmBitsPixel != 32) return 0;
+
+    if (!(src = CreateCompatibleDC( 0 ))) return 0;
+    if (!(dst = CreateCompatibleDC( src ))) goto done;
+    if (!(info = HeapAlloc( GetProcessHeap(), 0, FIELD_OFFSET( BITMAPINFO, bmiColors[256] )))) goto done;
+    info->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    info->bmiHeader.biWidth = bm.bmWidth;
+    info->bmiHeader.biHeight = -bm.bmHeight;
+    info->bmiHeader.biPlanes = 1;
+    info->bmiHeader.biBitCount = 32;
+    info->bmiHeader.biCompression = BI_RGB;
+    info->bmiHeader.biSizeImage = bm.bmWidth * bm.bmHeight * 4;
+    info->bmiHeader.biXPelsPerMeter = 0;
+    info->bmiHeader.biYPelsPerMeter = 0;
+    info->bmiHeader.biClrUsed = 0;
+    info->bmiHeader.biClrImportant = 0;
+    if (!(alpha = CreateDIBSection( dst, info, DIB_RGB_COLORS, &bits, NULL, 0 ))) goto done;
+
+    SelectObject( src, hbitmap );
+    SelectObject( dst, alpha );
+    BitBlt(dst, 0, 0, bm.bmWidth, bm.bmHeight, src, 0, 0, SRCCOPY);
+
+    for (i = 0, ptr = bits; i < bm.bmWidth * bm.bmHeight; i++, ptr += 4)
+        if ((has_alpha = (ptr[3] != 0))) break;
+
+done:
+    DeleteDC( src );
+    DeleteDC( dst );
+    HeapFree( GetProcessHeap(), 0, info );
+
+    if (!has_alpha)
+    {
+        DeleteObject( alpha );
+        alpha = 0;
+    }
+    return alpha;
+}
+
 /***********************************************************************
  *           STATIC_SetBitmap
  *
@@ -145,7 +221,8 @@ static HICON STATIC_SetIcon( HWND hwnd, HICON hicon, DWORD style )
  */
 static HBITMAP STATIC_SetBitmap( HWND hwnd, HBITMAP hBitmap, DWORD style )
 {
-    HBITMAP hOldBitmap;
+    HBITMAP hOldBitmap, alpha;
+    struct static_extra_info *extra;
 
     if ((style & SS_TYPEMASK) != SS_BITMAP) return 0;
     if (hBitmap && GetObjectType(hBitmap) != OBJ_BITMAP)
@@ -153,7 +230,23 @@ static HBITMAP STATIC_SetBitmap( HWND hwnd, HBITMAP hBitmap, DWORD style )
         WARN("hBitmap != 0, but it's not a bitmap\n");
         return 0;
     }
-    hOldBitmap = (HBITMAP)SetWindowLongPtrW( hwnd, HICON_GWL_OFFSET, (LONG_PTR)hBitmap );
+
+    extra = get_extra_ptr( hwnd, TRUE );
+    if (!extra) return 0;
+
+    hOldBitmap = extra->image.hbitmap;
+    alpha = create_alpha_bitmap( hBitmap );
+    if (alpha)
+    {
+        extra->image.hbitmap = alpha;
+        extra->image_has_alpha = TRUE;
+    }
+    else
+    {
+        extra->image.hbitmap = hBitmap;
+        extra->image_has_alpha = FALSE;
+    }
+
     if (hBitmap && !(style & SS_CENTERIMAGE) && !(style & SS_REALSIZECONTROL))
     {
         BITMAP bm;
@@ -183,13 +276,23 @@ static HBITMAP STATIC_SetBitmap( HWND hwnd, HBITMAP hBitmap, DWORD style )
  */
 static HENHMETAFILE STATIC_SetEnhMetaFile( HWND hwnd, HENHMETAFILE hEnhMetaFile, DWORD style )
 {
+    HENHMETAFILE old_hemf;
+    struct static_extra_info *extra;
+
     if ((style & SS_TYPEMASK) != SS_ENHMETAFILE) return 0;
     if (hEnhMetaFile && GetObjectType(hEnhMetaFile) != OBJ_ENHMETAFILE)
     {
         WARN("hEnhMetaFile != 0, but it's not an enhanced metafile\n");
         return 0;
     }
-    return (HENHMETAFILE)SetWindowLongPtrW( hwnd, HICON_GWL_OFFSET, (LONG_PTR)hEnhMetaFile );
+
+    extra = get_extra_ptr( hwnd, TRUE );
+    if (!extra) return 0;
+
+    old_hemf = extra->image.hemf;
+    extra->image.hemf = hEnhMetaFile;
+
+    return old_hemf;
 }
 
 /***********************************************************************
@@ -200,6 +303,8 @@ static HENHMETAFILE STATIC_SetEnhMetaFile( HWND hwnd, HENHMETAFILE hEnhMetaFile,
  */
 static HANDLE STATIC_GetImage( HWND hwnd, WPARAM wParam, DWORD style )
 {
+    struct static_extra_info *extra;
+
     switch (style & SS_TYPEMASK)
     {
         case SS_ICON:
@@ -215,7 +320,22 @@ static HANDLE STATIC_GetImage( HWND hwnd, WPARAM wParam, DWORD style )
         default:
             return NULL;
     }
-    return (HANDLE)GetWindowLongPtrW( hwnd, HICON_GWL_OFFSET );
+
+    extra = get_extra_ptr( hwnd, FALSE );
+    return extra ? extra->image.hbitmap : 0;
+}
+
+static void STATIC_SetFont( HWND hwnd, HFONT hfont )
+{
+    struct static_extra_info *extra = get_extra_ptr( hwnd, TRUE );
+    if (extra)
+        extra->hfont = hfont;
+}
+
+static HFONT STATIC_GetFont( HWND hwnd )
+{
+    struct static_extra_info *extra = get_extra_ptr( hwnd, FALSE );
+    return extra ? extra->hfont : 0;
 }
 
 /***********************************************************************
@@ -327,6 +447,13 @@ static LRESULT CALLBACK STATIC_WindowProc( HWND hwnd, UINT uMsg, WPARAM wParam, 
     case WM_NCDESTROY:
         if (style == SS_ICON)
         {
+            struct static_extra_info *extra = get_extra_ptr( hwnd, FALSE );
+            if (extra)
+            {
+                if (extra->image_has_alpha)
+                    DeleteObject( extra->image.hbitmap );
+                heap_free( extra );
+            }
 /*
  * FIXME
  *           DestroyIcon32( STATIC_SetIcon( wndPtr, 0 ) );
@@ -423,14 +550,14 @@ static LRESULT CALLBACK STATIC_WindowProc( HWND hwnd, UINT uMsg, WPARAM wParam, 
     case WM_SETFONT:
         if (hasTextStyle( full_style ))
         {
-            SetWindowLongPtrW( hwnd, HFONT_GWL_OFFSET, wParam );
+            STATIC_SetFont( hwnd, (HFONT)wParam );
             if (LOWORD(lParam))
                 RedrawWindow( hwnd, NULL, 0, RDW_INVALIDATE | RDW_ERASE | RDW_UPDATENOW | RDW_ALLCHILDREN );
         }
         break;
 
     case WM_GETFONT:
-        return GetWindowLongPtrW( hwnd, HFONT_GWL_OFFSET );
+        return (LRESULT)STATIC_GetFont( hwnd );
 
     case WM_NCHITTEST:
         if (full_style & SS_NOTIFY)
@@ -508,7 +635,7 @@ static void STATIC_PaintOwnerDrawfn( HWND hwnd, HDC hdc, DWORD style )
     dis.itemData   = 0;
     GetClientRect( hwnd, &dis.rcItem );
 
-    font = (HFONT)GetWindowLongPtrW( hwnd, HFONT_GWL_OFFSET );
+    font = STATIC_GetFont( hwnd );
     if (font) oldFont = SelectObject( hdc, font );
     SendMessageW( GetParent(hwnd), WM_CTLCOLORSTATIC, (WPARAM)hdc, (LPARAM)hwnd );
     SendMessageW( GetParent(hwnd), WM_DRAWITEM, id, (LPARAM)&dis );
@@ -572,7 +699,7 @@ static void STATIC_PaintTextfn( HWND hwnd, HDC hdc, DWORD style )
             format |= DT_SINGLELINE | DT_WORD_ELLIPSIS;
     }
 
-    if ((hFont = (HFONT)GetWindowLongPtrW( hwnd, HFONT_GWL_OFFSET )))
+    if ((hFont = STATIC_GetFont( hwnd )))
         hOldFont = SelectObject( hdc, hFont );
 
     /* SS_SIMPLE controls: WM_CTLCOLORSTATIC is sent, but the returned
@@ -668,7 +795,7 @@ static void STATIC_PaintIconfn( HWND hwnd, HDC hdc, DWORD style )
 
     GetClientRect( hwnd, &rc );
     hbrush = STATIC_SendWmCtlColorStatic(hwnd, hdc);
-    hIcon = (HICON)GetWindowLongPtrW( hwnd, HICON_GWL_OFFSET );
+    hIcon = STATIC_GetImage( hwnd, IMAGE_ICON, style );
     if (!hIcon || !get_icon_size( hIcon, &size ))
     {
         FillRect(hdc, &rc, hbrush);
@@ -698,13 +825,15 @@ static void STATIC_PaintBitmapfn(HWND hwnd, HDC hdc, DWORD style )
 
     hbrush = STATIC_SendWmCtlColorStatic(hwnd, hdc);
 
-    if ((hBitmap = (HBITMAP)GetWindowLongPtrW( hwnd, HICON_GWL_OFFSET ))
+    if ((hBitmap = STATIC_GetImage( hwnd, IMAGE_BITMAP, style ))
          && (GetObjectType(hBitmap) == OBJ_BITMAP)
          && (hMemDC = CreateCompatibleDC( hdc )))
     {
         BITMAP bm;
         RECT rcClient;
         LOGBRUSH brush;
+        BLENDFUNCTION blend = { AC_SRC_OVER, 0, 255, AC_SRC_ALPHA };
+        struct static_extra_info *extra = get_extra_ptr( hwnd, FALSE );
 
         GetObjectW(hBitmap, sizeof(bm), &bm);
         oldbitmap = SelectObject(hMemDC, hBitmap);
@@ -725,7 +854,13 @@ static void STATIC_PaintBitmapfn(HWND hwnd, HDC hdc, DWORD style )
             rcClient.right = rcClient.left + bm.bmWidth;
             rcClient.bottom = rcClient.top + bm.bmHeight;
         }
-        StretchBlt(hdc, rcClient.left, rcClient.top, rcClient.right - rcClient.left,
+
+        if (extra->image_has_alpha)
+            GdiAlphaBlend(hdc, rcClient.left, rcClient.top, rcClient.right - rcClient.left,
+                   rcClient.bottom - rcClient.top, hMemDC,
+                   0, 0, bm.bmWidth, bm.bmHeight, blend);
+        else
+            StretchBlt(hdc, rcClient.left, rcClient.top, rcClient.right - rcClient.left,
                    rcClient.bottom - rcClient.top, hMemDC,
                    0, 0, bm.bmWidth, bm.bmHeight, SRCCOPY);
         SelectObject(hMemDC, oldbitmap);
@@ -742,7 +877,7 @@ static void STATIC_PaintEnhMetafn(HWND hwnd, HDC hdc, DWORD style )
     GetClientRect(hwnd, &rc);
     hbrush = STATIC_SendWmCtlColorStatic(hwnd, hdc);
     FillRect(hdc, &rc, hbrush);
-    if ((hEnhMetaFile = (HENHMETAFILE)GetWindowLongPtrW( hwnd, HICON_GWL_OFFSET )))
+    if ((hEnhMetaFile = STATIC_GetImage( hwnd, IMAGE_ENHMETAFILE, style )))
     {
         /* The control's current font is not selected into the
            device context! */
@@ -779,7 +914,7 @@ void STATIC_Register(void)
     wndClass.style = CS_DBLCLKS | CS_PARENTDC | CS_GLOBALCLASS;
     wndClass.lpfnWndProc = STATIC_WindowProc;
     wndClass.cbClsExtra = 0;
-    wndClass.cbWndExtra = STATIC_EXTRA_BYTES;
+    wndClass.cbWndExtra = sizeof(struct static_extra_info *);
     wndClass.hCursor = LoadCursorW(0, (LPWSTR)IDC_ARROW);
     wndClass.hbrBackground = NULL;
     wndClass.lpszClassName = WC_STATICW;

dlls/ntdll/file.c
@@ -203,7 +203,8 @@ int get_file_info( const char *path, struct stat *st, ULONG *attr )
     {
         BOOL is_dir;
 
-        stat(path, st);
+        /* return information about the destination (unless this is a dangling symlink) */
+        stat( path, st );
         /* symbolic links always report size 0 */
         st->st_size = 0;
         /* symbolic links (either junction points or NT symlinks) are "reparse points" */

dlls/winebus.sys/bus_sdl.c
@@ -123,7 +123,7 @@ struct platform_private
     int button_start;
     int axis_start;
     int ball_start;
-    int hat_bit_offs; /* hatswitches are reported in the same bytes as buttons */
+    int hat_start;
 
     int report_descriptor_size;
     BYTE *report_descriptor;
@@ -164,6 +164,10 @@ static const BYTE CONTROLLER_BUTTONS[] = {
     0x95, CONTROLLER_NUM_BUTTONS, /* REPORT_COUNT (11) */
     0x75, 0x01, /* REPORT_SIZE (1) */
     0x81, 0x02, /* INPUT (Data,Var,Abs) */
+    /* padding */
+    0x95, 0x05, /* REPORT_COUNT (5) */
+    0x75, 0x01, /* REPORT_SIZE (1) */
+    0x81, 0x03, /* INPUT (Cnst,Var,Abs) */
 };
 
 static const BYTE CONTROLLER_AXIS [] = {
@@ -298,50 +302,22 @@ static void set_ball_value(struct platform_private *ext, int index, int value1, 
 
 static void set_hat_value(struct platform_private *ext, int index, int value)
 {
-    int byte = ext->button_start + (ext->hat_bit_offs + 4 * index) / 8;
-    int bit_offs = (ext->hat_bit_offs + 4 * index) % 8;
-    int num_low_bits, num_high_bits;
-    unsigned char val, low_mask, high_mask;
-
-    /* 4-bit hatswitch value is packed into button bytes */
-    if (bit_offs <= 4)
-    {
-        num_low_bits = 4;
-        num_high_bits = 0;
-        low_mask = 0xf;
-        high_mask = 0;
-    }
-    else
-    {
-        num_low_bits = 8 - bit_offs;
-        num_high_bits = 4 - num_low_bits;
-        low_mask = (1 << num_low_bits) - 1;
-        high_mask = (1 << num_high_bits) - 1;
-    }
-
+    int offset;
+    offset = ext->hat_start + index;
     switch (value)
     {
         /* 8 1 2
          * 7 0 3
          * 6 5 4 */
-        case SDL_HAT_CENTERED: val = 0; break;
-        case SDL_HAT_UP: val = 1; break;
-        case SDL_HAT_RIGHTUP: val = 2; break;
-        case SDL_HAT_RIGHT: val = 3; break;
-        case SDL_HAT_RIGHTDOWN: val = 4; break;
-        case SDL_HAT_DOWN: val = 5; break;
-        case SDL_HAT_LEFTDOWN: val = 6; break;
-        case SDL_HAT_LEFT: val = 7; break;
-        case SDL_HAT_LEFTUP: val = 8; break;
-        default: return;
-    }
-
-    ext->report_buffer[byte] &= ~(low_mask << bit_offs);
-    ext->report_buffer[byte] |= (val & low_mask) << bit_offs;
-    if (high_mask)
-    {
-        ext->report_buffer[byte + 1] &= ~high_mask;
-        ext->report_buffer[byte + 1] |= val & high_mask;
+        case SDL_HAT_CENTERED: ext->report_buffer[offset] = 0; break;
+        case SDL_HAT_UP: ext->report_buffer[offset] = 1; break;
+        case SDL_HAT_RIGHTUP: ext->report_buffer[offset] = 2; break;
+        case SDL_HAT_RIGHT: ext->report_buffer[offset] = 3; break;
+        case SDL_HAT_RIGHTDOWN: ext->report_buffer[offset] = 4; break;
+        case SDL_HAT_DOWN: ext->report_buffer[offset] = 5; break;
+        case SDL_HAT_LEFTDOWN: ext->report_buffer[offset] = 6; break;
+        case SDL_HAT_LEFT: ext->report_buffer[offset] = 7; break;
+        case SDL_HAT_LEFTUP: ext->report_buffer[offset] = 8; break;
     }
 }
 
@@ -447,16 +423,18 @@ static BOOL build_report_descriptor(struct platform_private *ext)
     if (button_count)
     {
         descript_size += sizeof(REPORT_BUTTONS);
+        if (button_count % 8)
+            descript_size += sizeof(REPORT_PADDING);
+        report_size += (button_count + 7) / 8;
     }
 
     hat_count = pSDL_JoystickNumHats(ext->sdl_joystick);
-    ext->hat_bit_offs = button_count;
+    ext->hat_start = report_size;
     if (hat_count)
     {
         descript_size += sizeof(REPORT_HATSWITCH);
+        report_size += hat_count;
     }
-
-    report_size += (button_count + hat_count * 4 + 7) / 8;
 
     descript_size += test_haptic(ext);
 
@@ -490,6 +468,11 @@ static BOOL build_report_descriptor(struct platform_private *ext)
     if (button_count)
     {
         report_ptr = add_button_block(report_ptr, 1, button_count);
+        if (button_count % 8)
+        {
+            BYTE padding = 8 - (button_count % 8);
+            report_ptr = add_padding_block(report_ptr, padding);
+        }
     }
     if (hat_count)
         report_ptr = add_hatswitch(report_ptr, hat_count);
@@ -557,11 +540,9 @@ static BOOL build_mapped_report_descriptor(struct platform_private *ext)
 
     ext->axis_start = 0;
     ext->button_start = CONTROLLER_NUM_AXES * sizeof(WORD);
-    ext->hat_bit_offs = CONTROLLER_NUM_BUTTONS;
+    ext->hat_start = ext->button_start + (CONTROLLER_NUM_BUTTONS + 7) / 8;
 
-    ext->buffer_length = (CONTROLLER_NUM_BUTTONS + CONTROLLER_NUM_HATSWITCHES * 4 + 7) / 8
-        + CONTROLLER_NUM_AXES * sizeof(WORD)
-        + 2/* unknown constant*/;
+    ext->buffer_length = (CONTROLLER_NUM_BUTTONS + 7) / 8 + CONTROLLER_NUM_AXES * sizeof(WORD) + CONTROLLER_NUM_HATSWITCHES;
 
     TRACE("Report Descriptor will be %i bytes\n", descript_size);
     TRACE("Report will be %i bytes\n", ext->buffer_length);
@@ -585,7 +566,6 @@ static BOOL build_mapped_report_descriptor(struct platform_private *ext)
     memcpy(report_ptr, CONTROLLER_BUTTONS, sizeof(CONTROLLER_BUTTONS));
     report_ptr += sizeof(CONTROLLER_BUTTONS);
     report_ptr = add_hatswitch(report_ptr, 1);
-    report_ptr = add_padding_block(report_ptr, 16);/* unknown constant */
     report_ptr += build_haptic(ext, report_ptr);
     memcpy(report_ptr, REPORT_TAIL, sizeof(REPORT_TAIL));
 
@@ -603,10 +583,6 @@ static BOOL build_mapped_report_descriptor(struct platform_private *ext)
         set_axis_value(ext, i, pSDL_GameControllerGetAxis(ext->sdl_controller, i));
 
     set_hat_value(ext, 0, compose_dpad_value(ext->sdl_controller));
-
-    /* unknown constant */
-    ext->report_buffer[12] = 0x89;
-    ext->report_buffer[13] = 0xc5;
 
     return TRUE;
 }

dlls/winebus.sys/controller.h
@@ -79,7 +79,7 @@ static const BYTE REPORT_HATSWITCH[] = {
     0x25, 0x08,  /* LOGICAL_MAXIMUM (0x08) */
     0x35, 0x00,  /* PHYSICAL_MINIMUM (0) */
     0x45, 0x08,  /* PHYSICAL_MAXIMUM (8) */
-    0x75, 0x04,  /* REPORT_SIZE (4) */
+    0x75, 0x08,  /* REPORT_SIZE (8) */
     0x95, 0x01,  /* REPORT_COUNT (1) */
     0x81, 0x02,  /* INPUT (Data,Var,Abs) */
 };

libs/wine/Makefile.in
@@ -24,7 +24,7 @@ libwine_LDFLAGS = $(LIBWINE_LDFLAGS)
 libwine_DEPS = $(LIBWINE_DEPENDS)
 
 version.c: dummy
-	version=`(echo "wine-4.8.r0.g3134cc0b") | sed -n -e '$$s/\(.*\)/const char wine_build[] = "\1  ( TkG Staging Esync )";/p'` && (echo $$version | cmp -s - $@) || echo $$version >$@ || (rm -f $@ && exit 1)
+	version=`(echo "wine-4.8.r2.g6e49a945") | sed -n -e '$$s/\(.*\)/const char wine_build[] = "\1  ( TkG Staging Esync )";/p'` && (echo $$version | cmp -s - $@) || echo $$version >$@ || (rm -f $@ && exit 1)
 
 dummy:
 .PHONY: dummy

libs/wine/config.c
@@ -735,6 +735,8 @@ wine_patch_data[] =
     { "Daniel Jelinski", "wine.inf: Add registry keys for Windows Performance Library.", 1 },
     { "Daniel Wendt", "gdi32: Fix for rotated Arc, ArcTo, Chord and Pie drawing problem.", 1 },
     { "Daniel Wendt", "gdi32: Fix for rotated ellipse.", 1 },
+    { "Dmitry Timoshkov", "comctl32: Paint 32-bpp bitmaps with an alpha channel using GdiAlphaBlend.", 1 },
+    { "Dmitry Timoshkov", "comctl32: Switch to using a structure for extra storage.", 1 },
     { "Dmitry Timoshkov", "comdlg32: Postpone setting ofn->lpstrFileTitle to work around an application bug.", 1 },
     { "Dmitry Timoshkov", "gdiplus/tests: Add some tests for loading TIFF images in various color formats.", 1 },
     { "Dmitry Timoshkov", "gdiplus: Add support for more image color formats.", 1 },